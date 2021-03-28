71b7d5fce46aeb6123e0db5a34afc6712a99167f
Yuki Waka <yuheey3@yahoo.co.jp>
2021-03-28 19:19:33 -0400

    Post request

--- SmartShopping/RegistrationViewController.swift
+++ SmartShopping/RegistrationViewController.swift
@@ -9,6 +9,8 @@ import UIKit
 
 class RegistrationViewController: UIViewController {
     
+    let url = URL(string: "https://sundaland.herokuapp.com/api/users")!
+    
     
     
     @IBOutlet var tfName: UITextField!
@@ -23,6 +25,7 @@ class RegistrationViewController: UIViewController {
         
         self.navigationItem.title = "Registration"
         
+        
         // Do any additional setup after loading the view.
     }
     
@@ -30,7 +33,7 @@ class RegistrationViewController: UIViewController {
         self.view.endEditing(true)
     }
     
-
+    
     @IBAction func createAccount(){
         
         if (!self.tfEmail.text!.isEmpty && !self.tfName.text!.isEmpty && !self.tfPassword.text!.isEmpty && !self.tfConfirmPassword.text!.isEmpty) {
@@ -49,6 +52,9 @@ class RegistrationViewController: UIViewController {
                             
                             var check: String
                             check = self.segIsOwner.titleForSegment(at: self.segIsOwner.selectedSegmentIndex)!
+                            
+                            //fetch to database
+                            submitToDatabase()
                             
                             if(check == "Owner"){
                                 newUser.isOwner = true
@@ -120,8 +126,55 @@ class RegistrationViewController: UIViewController {
     }
     
     
+    func submitToDatabase() {
+        
+        //declare parameter as a dictionary which contains string as key and value combination. considering inputs are valid
+        
+        let parameters = ["name": newUser.name, "password": newUser.password, "email" : newUser.email] as Dictionary<String, String>
+        
+        //create the session object
+        let session = URLSession.shared
+        
+        //now create the URLRequest object using the url object
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST" //set http method as POST
+        
+        do {
+            request.httpBody = try JSONSerialization.data(withJSONObject: parameters, options: .prettyPrinted) // pass dictionary to nsdata object and set it as request body
+            
+        } catch let error {
+            print(error.localizedDescription)
+        }
+        
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.addValue("application/json", forHTTPHeaderField: "Accept")
+        
+        //create dataTask using the session object to send data to the server
+        let task = session.dataTask(with: request as URLRequest, completionHandler: { data, response, error in
+            
+            guard error == nil else {
+                return
+            }
+            
+            guard let data = data else {
+                return
+            }
+            
+            do {
+                //create json object from data
+                if let json = try JSONSerialization.jsonObject(with: data, options: .mutableContainers) as? [String: Any] {
+                    print(json)
+                    // handle json...
+                }
+                
+            } catch let error {
+                print(error.localizedDescription)
+            }
+        })
+        task.resume()
+        
+        
+        
+    }
+    
 }
-
-
-
-
--- SmartShopping/ViewController.swift
+++ SmartShopping/ViewController.swift
@@ -42,5 +42,6 @@ class ViewController: UIViewController {
         }
     }
     
+    
 }
 
